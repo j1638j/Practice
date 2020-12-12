7df25876eef1846cc3c31cb1f25836b0cd06b6c2
Jiwan Kim <j1638j@gmail.com>
2020-12-12 16:31:15 +0900

    Score Keeper completed: JS

---
+++ javascript practice/score_keeper_codeAlong.js
@@ -0,0 +1,49 @@
+const p1Btn = document.querySelector('#p1Button');
+const p2Btn = document.querySelector('#p2Button');
+const resetBtn = document.querySelector('#reset');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+const playTo = document.querySelector('#playto');
+
+let p1Score = 0;
+let p2Score = 0;
+let playToValue = playTo.value;
+
+function addScore(player) {
+
+    if (player === 1) {
+        p1Score++;
+        p1Display.innerText = p1Score;
+    } else if (player === 2) {
+        p2Score++;
+        p2Display.innerText = p2Score;
+    }
+
+    if (p1Score == playToValue || p2Score == playToValue) {
+        //make buttons unclickable
+        p1Btn.disabled = true;
+        p2Btn.disabled = true;
+    }
+
+}
+
+p1Btn.addEventListener('click', () => { addScore(1) });
+p2Btn.addEventListener('click', () => { addScore(2) });
+
+playTo.addEventListener('change', (event) => {
+    playToValue = playTo.value;
+})
+
+resetBtn.addEventListener('click', () => {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.innerText = 0;
+    p2Display.innerText = 0;
+
+    //make buttons clickable again
+    p1Btn.disabled = false;
+    p2Btn.disabled = false;
+
+})
+
+
